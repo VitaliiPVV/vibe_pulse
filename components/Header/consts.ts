cd321c131f4867ae933e9aabd7f865f2ef59949a
VitaliiPVV <vitalii_pavlov@pecodesoftware.com>
2025-12-17 16:50:51 +0200

    added AI integration, created dashboard page with info about entries

--- components/Header/consts.ts
+++ components/Header/consts.ts
@@ -6,11 +6,16 @@ export const LINKS = [
   },
   {
     id: 2,
+    name: 'Dashboard',
+    link: '/dashboard',
+  },
+  {
+    id: 3,
     name: 'Pricing',
     link: 'pricing',
   },
   {
-    id: 3,
+    id: 4,
     name: 'My account',
     link: '/account',
   },
