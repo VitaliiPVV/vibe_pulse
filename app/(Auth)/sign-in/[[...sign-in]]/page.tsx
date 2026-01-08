5fb30e7b217b6f9209e2d28e36df40c96b7c809e
VitaliiPVV <vitalii_pavlov@pecodesoftware.com>
2026-01-08 15:29:09 +0200

    fixed issue with clerk

--- app/(Auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(Auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,6 @@ export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <SignIn
-        afterSignInUrl="/journal"
         appearance={{
           layout: {
             unsafe_disableDevelopmentModeWarnings: true,
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: '/sign-in',
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: '/sign-up',
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: '/journal',
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: '/journal',
+  },
 };
 
 export default nextConfig;
